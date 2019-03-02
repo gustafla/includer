a94c91f3ff387d68d0dfdbbbbc55421f05667a46
Lauri Gustafsson <luutifa@jkry.org>
2019-03-03 01:48:18 +0200

    Add tests for inclusion guards

--- test/test1.h
+++ test/test1.h
@@ -1,5 +1,10 @@
+#ifndef TEST1_H
+#define TEST1_H
+
 #include "test2.h"
 
 void test1() {
 	test2();
 }
+
+#endif
--- test/test2.h
+++ test/test2.h
@@ -1,5 +1,11 @@
-#include <iostream>
+#ifndef TEST2_H
+#define TEST2_H
+
+#include <stdio.h>
+#include "test1.h"
 
 void test2() {
 	printf("Hello world\n");
 }
+
+#endif
