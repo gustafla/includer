683435a63b72b07f38b269daca967dc1293780c8
Lauri Gustafsson <luutifa@jkry.org>
2019-03-05 01:19:07 +0200

    Add not-yet-working code for include guard support

--- src/process_includes.c
+++ src/process_includes.c
@@ -4,19 +4,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include "uthash.h"
+
+#define MACRO_NAME_LEN 64
+
+struct name {
+	char name[MACRO_NAME_LEN];
+	UT_hash_handle hh;
+};
 
 bool cmp_is_not_quotation(char c) {
 	return c != '\"';
 }
 
-int seek_line(char **c, bool(*cmp)(char)) {
+bool cmp_is_alpha_or_underscore(char c) {
+	return isalpha(c) || c == '_';
+}
+
+bool cmp_is_not_eol(char c) {
+	return c != '\n';
+}
+
+bool cmp_is_space_or_tab(char c) {
+	return c == ' ' || c == '\t';
+}
+
+include_result_t seek_line(char **c, bool(*cmp)(char)) {
 	while (cmp(**c)) {
 		if (**c == '\0' || **c == '\n') {
-			return EXIT_FAILURE;
+			return INCLUDE_ERR;
 		}
 		(*c)++;
 	}
-	return EXIT_SUCCESS;
+	return INCLUDE_OK;
 }
 
 void append(char **buffer, char *source, size_t *bufsize, size_t len) {
@@ -35,12 +56,14 @@ include_result_t local_include(
 
 	// find end of filename
 	char *f = q + 1;
-	seek_line(&f, cmp_is_not_quotation);
+	if (seek_line(&f, cmp_is_not_quotation) == INCLUDE_ERR) {
+		return INCLUDE_ERR;
+	}
 
 	// allocate and set up a filename buffer
-	size_t len_filename = f - (q + 1);
-	char *filename = (char*)calloc(sizeof(char), len_filename + 1);
-	strncpy(filename, q + 1, len_filename);
+	size_t filename_len = f - (q + 1);
+	char *filename = (char*)calloc(sizeof(char), filename_len + 1);
+	strncpy(filename, q + 1, filename_len);
 
 	// read the file that needs to be included
 	char *str = NULL;
@@ -51,12 +74,12 @@ include_result_t local_include(
 		// recursively process its includes
 		fprintf(stderr, "Processing %s\n", filename);
 		char *processed = NULL;
-		size_t len_processed = 0;
-		if (process_includes(&processed, &len_processed, str) == INCLUDE_OK) {
+		size_t processed_len = 0;
+		if (process_includes(&processed, &processed_len, str) == INCLUDE_OK) {
 			fprintf(stderr, "Finished %s\n", filename);
 
 			// append to output buffer
-			append(buffer, processed, bufsize, len_processed);
+			append(buffer, processed, bufsize, processed_len);
 
 			// mark last place before which did processing
 			while (*f != '\n' && *f != '\0') f++;
@@ -71,16 +94,50 @@ include_result_t local_include(
 	return rc;
 }
 
+size_t min(size_t a, size_t b) {
+	if (a < b) {
+		return a;
+	}
+	return b;
+}
+
+include_result_t get_macro_name(char *dst, char *c) {
+	// seek for macro name start
+	if (seek_line(&c, cmp_is_space_or_tab) == INCLUDE_ERR) {
+		return INCLUDE_ERR;
+	}
+	char *macro = c;
+
+	// seek for macro name end
+	if (seek_line(&c, cmp_is_alpha_or_underscore) == INCLUDE_ERR) {
+		return INCLUDE_ERR;
+	}
+	size_t macro_len = min(c - macro, MACRO_NAME_LEN - 1);
+
+	// write to output and terminate
+	memcpy(dst, macro, macro_len);
+	dst[macro_len] = '\0';
+
+	return INCLUDE_OK;
+}
+
 #define KW_INCLUDE "#include"
+#define KW_INCLUDE_LEN (sizeof(KW_INCLUDE) - 1)
 #define KW_DEFINE "#define"
+#define KW_DEFINE_LEN (sizeof(KW_DEFINE) - 1)
 #define KW_ENDIF "#endif"
+#define KW_ENDIF_LEN (sizeof(KW_ENDIF) - 1)
 #define KW_IF "#if"
+#define KW_IF_LEN (sizeof(KW_IF) - 1)
 #define NDEF "ndef"
+#define NDEF_LEN (sizeof(NDEF) - 1)
 
 include_result_t process_includes(char **dst, size_t *len, char *sourcecode) {
-	int linen = 1;
+	include_result_t rc = INCLUDE_OK;
+	int linen = 1, skip = 0;
 	bool newline = true;
 	char *lastpoint = sourcecode;
+	struct name *macro_name, *name_tmp, *macro_names = NULL;
 
 	// allow for NULL len parameter
 	size_t local_len = 0;
@@ -93,18 +150,55 @@ include_result_t process_includes(char **dst, size_t *len, char *sourcecode) {
 			continue;
 		} else if (newline && *i == '#') {
 			fprintf(stderr, "Preprocessor directive at line %d\n", linen);
-
-			if (strncmp(i, KW_IF, sizeof(KW_IF) - 1) == 0) {
+			if (strncmp(i, KW_IF, KW_IF_LEN) == 0) {
 				fprintf(stderr, "...which was an if\n");
 
-			} else if (strncmp(i, KW_ENDIF, sizeof(KW_ENDIF) - 1) == 0) {
+				if (strncmp(i + KW_IF_LEN, NDEF, NDEF_LEN) == 0) {
+					fprintf(stderr, "...of type ifndef\n");
+
+					// initialize a name compare buffer
+					char compare[MACRO_NAME_LEN];
+					char *c = i + KW_IF_LEN + NDEF_LEN + 1;
+					if (get_macro_name(compare, c) == EXIT_FAILURE) {
+						rc = INCLUDE_ERR;
+						goto cleanup;
+					}
+
+					// check if defined
+					HASH_FIND_STR(macro_names, compare, macro_name);
+					if (macro_name) {
+						append(dst, lastpoint, len, (size_t)(i - lastpoint));
+						skip++;
+					}
+				} else {
+					if (skip) skip++;
+				}
+			} else if (strncmp(i, KW_ENDIF, KW_ENDIF_LEN) == 0) {
 				fprintf(stderr, "...which was an endif\n");
-	
-			} else if (strncmp(i, KW_DEFINE, sizeof(KW_DEFINE) - 1) == 0) {
+
+				if (skip) {
+					skip--;
+					if (!skip) {
+						if (seek_line(&i, cmp_is_not_eol) == INCLUDE_ERR) {
+							rc = INCLUDE_ERR;
+							goto cleanup;
+						}
+						lastpoint = i + 1;
+					}
+				}
+			} else if (!skip && strncmp(i, KW_DEFINE, KW_DEFINE_LEN) == 0) {
 				fprintf(stderr, "...which was a define\n");
 	
-			} else if (strncmp(i, KW_INCLUDE, sizeof(KW_INCLUDE) - 1) == 0) {
+				macro_name = (struct name*)malloc(sizeof(struct name));
+				char *c = i + KW_DEFINE_LEN + 1;
+				if (get_macro_name(macro_name->name, c) == EXIT_FAILURE) {
+					rc = INCLUDE_ERR;
+					goto cleanup;
+				}
+				HASH_ADD_STR(macro_names, name, macro_name);
+			} else if (!skip && strncmp(i, KW_INCLUDE, KW_INCLUDE_LEN) == 0) {
 				fprintf(stderr, "...which was an include\n");
+
 				// seek whitespace until filename quotation
 				char *q = i + sizeof(KW_INCLUDE) - 1;
 				while (*q == ' ' || *q == '\t') q++;
@@ -117,8 +211,8 @@ include_result_t process_includes(char **dst, size_t *len, char *sourcecode) {
 					append(dst, lastpoint, len, (size_t)(i - lastpoint));
 
 					if (local_include(q, &lastpoint, dst, len) == INCLUDE_ERR) {
-						fprintf(stderr, "Can't process line %d\n", linen);
-						return INCLUDE_ERR;
+						rc = INCLUDE_ERR;
+						goto cleanup;
 					}
 				}
 			}
@@ -133,5 +227,15 @@ include_result_t process_includes(char **dst, size_t *len, char *sourcecode) {
 	*len += len_suffix;
 	(*dst)[*len] = '\0';
 
-	return INCLUDE_OK;
+cleanup:
+	if (rc != INCLUDE_OK) {
+		fprintf(stderr, "Can't process line %d\n", linen);
+	}
+
+	HASH_ITER(hh, macro_names, macro_name, name_tmp) {
+		HASH_DEL(macro_names, macro_name);
+		free(macro_name);
+	}
+
+	return rc;
 }
