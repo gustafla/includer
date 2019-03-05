304da849ca423a227646390e429e4703eb606ab6
Lauri Gustafsson <luutifa@jkry.org>
2019-03-05 11:11:11 +0200

    Fix bugs and now include guards work

--- src/process_includes.c
+++ src/process_includes.c
@@ -12,14 +12,14 @@
 struct name {
 	char name[MACRO_NAME_LEN];
 	UT_hash_handle hh;
-};
+} *macro_name, *name_tmp, *macro_names;
 
 bool cmp_is_not_quotation(char c) {
 	return c != '\"';
 }
 
-bool cmp_is_alpha_or_underscore(char c) {
-	return isalpha(c) || c == '_';
+bool cmp_is_alnum_or_underscore(char c) {
+	return isalnum(c) || c == '_';
 }
 
 bool cmp_is_not_eol(char c) {
@@ -45,6 +45,8 @@ void append(char **buffer, char *source, size_t *bufsize, size_t len) {
 	memcpy(*buffer + *bufsize, source, len);
 	*bufsize += len;
 }
+
+include_result_t process(char **dst, size_t *len, char *source);
 
 include_result_t local_include(
 		char *q,
@@ -75,7 +77,7 @@ include_result_t local_include(
 		fprintf(stderr, "Processing %s\n", filename);
 		char *processed = NULL;
 		size_t processed_len = 0;
-		if (process_includes(&processed, &processed_len, str) == INCLUDE_OK) {
+		if (process(&processed, &processed_len, str) == INCLUDE_OK) {
 			fprintf(stderr, "Finished %s\n", filename);
 
 			// append to output buffer
@@ -109,7 +111,7 @@ include_result_t get_macro_name(char *dst, char *c) {
 	char *macro = c;
 
 	// seek for macro name end
-	if (seek_line(&c, cmp_is_alpha_or_underscore) == INCLUDE_ERR) {
+	if (seek_line(&c, cmp_is_alnum_or_underscore) == INCLUDE_ERR) {
 		return INCLUDE_ERR;
 	}
 	size_t macro_len = min(c - macro, MACRO_NAME_LEN - 1);
@@ -132,18 +134,16 @@ include_result_t get_macro_name(char *dst, char *c) {
 #define NDEF "ndef"
 #define NDEF_LEN (sizeof(NDEF) - 1)
 
-include_result_t process_includes(char **dst, size_t *len, char *sourcecode) {
-	include_result_t rc = INCLUDE_OK;
+include_result_t process(char **dst, size_t *len, char *source) {
 	int linen = 1, skip = 0;
 	bool newline = true;
-	char *lastpoint = sourcecode;
-	struct name *macro_name, *name_tmp, *macro_names = NULL;
+	char *lastpoint = source;
 
 	// allow for NULL len parameter
 	size_t local_len = 0;
 	if (!len) len = &local_len;
 
-	for (char *i = sourcecode; *i; i++) {
+	for (char *i = source; *i; i++) {
 		if (*i == '\n') {
 			linen++;
 			newline = true;
@@ -159,9 +159,9 @@ include_result_t process_includes(char **dst, size_t *len, char *sourcecode) {
 					// initialize a name compare buffer
 					char compare[MACRO_NAME_LEN];
 					char *c = i + KW_IF_LEN + NDEF_LEN + 1;
-					if (get_macro_name(compare, c) == EXIT_FAILURE) {
-						rc = INCLUDE_ERR;
-						goto cleanup;
+					if (get_macro_name(compare, c) == INCLUDE_ERR) {
+						fprintf(stderr, "Can't make sense of line %d\n", linen);
+						return INCLUDE_ERR;
 					}
 
 					// check if defined
@@ -180,8 +180,8 @@ include_result_t process_includes(char **dst, size_t *len, char *sourcecode) {
 					skip--;
 					if (!skip) {
 						if (seek_line(&i, cmp_is_not_eol) == INCLUDE_ERR) {
-							rc = INCLUDE_ERR;
-							goto cleanup;
+							fprintf(stderr, "Line %d ended early\n", linen);
+							return INCLUDE_ERR;
 						}
 						lastpoint = i + 1;
 					}
@@ -191,9 +191,10 @@ include_result_t process_includes(char **dst, size_t *len, char *sourcecode) {
 	
 				macro_name = (struct name*)malloc(sizeof(struct name));
 				char *c = i + KW_DEFINE_LEN + 1;
-				if (get_macro_name(macro_name->name, c) == EXIT_FAILURE) {
-					rc = INCLUDE_ERR;
-					goto cleanup;
+				if (get_macro_name(macro_name->name, c) == INCLUDE_ERR) {
+					free(macro_name);
+					fprintf(stderr, "Can't get name from line %d\n", linen);
+					return INCLUDE_ERR;
 				}
 				HASH_ADD_STR(macro_names, name, macro_name);
 			} else if (!skip && strncmp(i, KW_INCLUDE, KW_INCLUDE_LEN) == 0) {
@@ -211,8 +212,8 @@ include_result_t process_includes(char **dst, size_t *len, char *sourcecode) {
 					append(dst, lastpoint, len, (size_t)(i - lastpoint));
 
 					if (local_include(q, &lastpoint, dst, len) == INCLUDE_ERR) {
-						rc = INCLUDE_ERR;
-						goto cleanup;
+						fprintf(stderr, "Include at line %d failed\n", linen);
+						return INCLUDE_ERR;
 					}
 				}
 			}
@@ -227,15 +228,18 @@ include_result_t process_includes(char **dst, size_t *len, char *sourcecode) {
 	*len += len_suffix;
 	(*dst)[*len] = '\0';
 
-cleanup:
-	if (rc != INCLUDE_OK) {
-		fprintf(stderr, "Can't process line %d\n", linen);
-	}
+	return INCLUDE_OK;
+}
+
+include_result_t process_includes(char **dst, size_t *len, char *source) {
+	macro_names = NULL;
+
+	include_result_t result = process(dst, len, source);
 
 	HASH_ITER(hh, macro_names, macro_name, name_tmp) {
 		HASH_DEL(macro_names, macro_name);
 		free(macro_name);
 	}
 
-	return rc;
+	return result;
 }
--- src/process_includes.h
+++ src/process_includes.h
@@ -8,6 +8,6 @@ typedef enum {
 	INCLUDE_ERR
 } include_result_t;
 
-include_result_t process_includes(char **dst, size_t *len, char *sourcecode);
+include_result_t process_includes(char **dst, size_t *len, char *source);
 
 #endif // PROCESS_INCLUDES_H
