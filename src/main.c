4b2f5fb86e6a794a1b25bae1d48b1305cb3beabc
Lauri Gustafsson <luutifa@jkry.org>
2019-03-07 03:00:52 +0200

    Fix string literal free bug

--- src/main.c
+++ src/main.c
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
 		if (main_path) {
 			arraylist_add(paths, (void*)main_path);
 		} else {
-			arraylist_add(paths, (void*)".");
+			arraylist_add(paths, (void*)copy_str("."));
 		}
 
 		char *source = NULL;
